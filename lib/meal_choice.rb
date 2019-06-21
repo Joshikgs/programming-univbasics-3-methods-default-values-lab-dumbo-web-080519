7bae8b6cb067c84c79b8d2937b51bfeb8e80c467
Josh Nho <joshnho@gmail.com>
2019-06-21 02:03:12 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -5,21 +5,3 @@ def meal_choice(veg1, veg2, protein = meat)
   puts "A plate of #{protein} with #{veg1} and #{veg2}."
 end
 
-
-require 'spec_helper'
-
-describe '#meal_choice' do
-  it 'should default to meat for the protein' do
-    expect(meal_choice("broccoli", "macaroni")).to eq("A plate of meat with broccoli and macaroni.")
-  end
-
-  it 'should allow you to set a protein' do
-    expect(meal_choice("broccoli", "macaroni", "tofu")).to eq("A plate of tofu with broccoli and macaroni.")
-  end
-
-  it 'should puts "What a nutritious meal!" and your order to the console' do
-    expect($stdout).to receive(:puts).with("What a nutritious meal!")
-    expect($stdout).to receive(:puts).with("A plate of meat with broccoli and macaroni.")
-    meal_choice("broccoli", "macaroni")
-  end
-end
